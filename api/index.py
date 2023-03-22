ab369dfb4899445cff524e6a0fed0bedaab50593
llc <15037115676@163.com>
2023-03-23 07:34:07 +0800

    add test

--- api/index.py
+++ api/index.py
@@ -19,6 +19,24 @@ def hello_world():  # put application's code here
     print(results)
     return results
 
+@app.route('/test', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        keywords = request.form.get('q')
+        max_results = int(request.form.get('max_results') or "3")
+    else:
+        keywords = request.args.get('q')
+        max_results = int(request.args.get('max_results') or "3")
+
+    # 示例 JSON 响应
+    results = {
+        "keywords": keywords,
+        "max_results": max_results,
+        "results": ["result1", "result2", "result3"]
+    }
+    return jsonify(results)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
