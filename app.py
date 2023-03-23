f433ef5bba500e99774d20bb7b2f6de3f3f345d2
ChenLu <chenlulu@drojian.dev>
2023-03-23 13:11:14 +0800

    code review

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from duckduckgo_search import ddg
 
 app = Flask(__name__)
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        keywords = request.args.get('q')
+        max_results = int(request.args.get('max_results') or "3")
+    elif request.method == 'POST':
+        data = request.get_json()
+        keywords = data.get('q')
+        max_results = int(data.get('max_results') or "3")
 
-@app.route('/search')
-def hello_world():  # put application's code here
-    from duckduckgo_search import ddg
-
-    keywords = request.args.get('q')
-    print(request.args.get('max_results'))
-    max_results = int(request.args.get('max_results') or "3")
     results = ddg(keywords, region='wt-wt', max_results=max_results)
     print(results)
-    return results
-
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
