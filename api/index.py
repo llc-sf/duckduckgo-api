5b27c282a53f9edf2aa6b2e1e79a84cfc6313e58
ChenLu <chenlulu@drojian.dev>
2023-03-23 12:38:04 +0800

    code review

--- api/index.py
+++ api/index.py
@@ -3,41 +3,20 @@ from duckduckgo_search import ddg
 
 app = Flask(__name__)
 
-
 @app.route('/search', methods=['GET', 'POST'])
-def hello_world():  # put application's code here
-   
-
-    if request.method == 'POST':
-        keywords = request.form.get('q')
-        max_results = int(request.form.get('max_results') or "3")
-    else:
+def search():
+    if request.method == 'GET':
         keywords = request.args.get('q')
         max_results = int(request.args.get('max_results') or "3")
+    elif request.method == 'POST':
+        data = request.get_json()
+        keywords = data.get('q')
+        max_results = int(data.get('max_results') or "3")
 
-    
     results = ddg(keywords, region='wt-wt', max_results=max_results)
     print(results)
-    return results
-
-@app.route('/test', methods=['POST'])
-def search():
-    if request.method == 'POST':
-        keywords = request.form.get('q')
-        max_results = int(request.form.get('max_results') or "3")
-    else:
-        keywords = request.args.get('q')
-        max_results = int(request.args.get('max_results') or "3")
-
-    # 示例 JSON 响应
-    results = {
-        "keywords": keywords,
-        "max_results": max_results,
-        "results": ["result1", "result2", "result3"]
-    }
     return jsonify(results)
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
